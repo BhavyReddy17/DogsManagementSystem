0ce2e1103978057462c637bc8181f1034aa9ca9e
Sanku <S562316@nwmissouri.edu>
2023-11-14 22:51:45 -0600

    intail commit

---
+++ src/main/java/com/bandiclasses/DogsManagementSystem/repository/DogRepository.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.bandiclasses.DogsManagementSystem.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.bandiclasses.DogsManagementSystem.Models.Dog;
+
+/**
+ * @author Bhavya Reddy Sanku S562316
+ * Nov 12, 2023
+ */
+public interface DogRepository extends CrudRepository<Dog, Integer>{
+	
+	
+
+}
---
+++ src/main/java/com/bandiclasses/DogsManagementSystem/repository/TrainerRepository.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.bandiclasses.DogsManagementSystem.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.bandiclasses.DogsManagementSystem.Models.Trainer;
+
+/**
+ * @author Bhavya Reddy Sanku S562316
+ * Nov 13, 2023
+ */
+public interface TrainerRepository extends CrudRepository<Trainer, Integer> {
+
+}
